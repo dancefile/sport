bdb6a181963500a91b8551909ba4af7c5d40ac64
mdolgopolov <32141496+mdolgopolov@users.noreply.github.com>
2017-12-11 13:13:18 +0300

    2

--- views/category/index.php
+++ views/category/index.php
@@ -18,25 +18,31 @@ $this->params['breadcrumbs'][] = $this->title;
     <p>
         <?= Html::a('Create Category', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
-<?php Pjax::begin(); ?>    <?= GridView::widget([
+
+<?php Pjax::begin(); ?>    
+    
+     
+    <?= GridView::widget([
         'dataProvider' => $dataProvider,
         'filterModel' => $searchModel,
         'columns' => [
+            'otd',
             'name',
             'clas',
             'program',
             'skay',
             'solo',
-            // 'otd',
             'agemin',
             'agemax',
             // 'age_id',            
             'dances',
-            'regPairs',
+           
 
             ['class' => 'yii\grid\ActionColumn'],
         ],
-    ]); ?>
+    ]);    ?>
+
+    
 
     <?= GridView::widget([
         'dataProvider' => $turDataProvider,
