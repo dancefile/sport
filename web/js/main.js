b0ecd0f7a9dd0ff8eef2a3b87e3b7217767a890e
mdolgopolov <32141496+mdolgopolov@users.noreply.github.com>
2018-01-13 15:50:43 +0200

    1

--- web/js/main.js
+++ web/js/main.js
@@ -2,30 +2,18 @@
 $(function(){
     $('#category-dances').sortable();
     $('#tur-dances').sortable();
-
     $('#category-chesses_list').sortable();
-    // if ($("#category-solo inpur:radio").value = 2) {alert('rere')};
-
-    // $("input:radio").click(function(){
-    // 	alert ('gfgfg');
-    // });
 });
 
-    
-
-// $(function () {
-//     $('#regButton').click(function(){
-//         $.post(
-//             [ "create", 
-//                 {
-//                     turList : $('#grid-reg').yiiGridView('getSelectedRows')
-//             },]
-
-//         );
-
-//     });
-
-// });
+function addField () {
+  
+    $( "#trener_1" ).clone().appendTo( "#trener_list" ).attr('id', 'trener_2');
+    //  $( "div.field-in-dancer_trener-0-name" ).clone().appendTo( "#trener_list" );
+    //  $("div#add_field_area").append("<div id="add" telnum "" class="add"><label> Поле №" telnum "</label><input type="text" width="120" name="val[]" id="val"  value=""/></div>");
+    var trener_num = parseInt($("#trener_list").find("div#trener_1:last").attr("id").slice(7));
+    alert (trener_num);
+  
+}
 
 $('.colaps').click(function(){
     $(this).nextUntil('tr.colaps').slideToggle(100);
