41479e8ec18d7a40a27e2b744fdaf26680710ce0
mdolgopolov <32141496+mdolgopolov@users.noreply.github.com>
2018-01-31 17:09:20 +0300

    1

--- controllers/ParserController.php
+++ controllers/ParserController.php
@@ -13,34 +13,87 @@ class ParserController extends Controller
 {
     public function actionIndex()
     {
-        include '..\components\simple_html_dom.php';
-//        ini_set("memory_limit","30M");
         if (Yii::$app->request->post()) {
             $start = Yii::$app->request->post('start');
             $end = Yii::$app->request->post('end');
             
-            while($start <= $end){
-                $html = file_get_html('http://www.danceleague.ru/db.php?id=e18073,c'.$start); // загружаем данные
-                if (count($html->find('.DbTableCell'))){
-                    foreach($html->find('p') as $a){
-                        $www = str_get_html($a);
-                        $www->find('td');
-                        echo $www;
+            require_once "..\components\simple_html_dom.php";       // подгружаем компонент для работы с DOM
+            $url = 'http://www.danceleague.ru/db.php?id=e18073,c';
+            $page = file_get_html($url);
+            $tr = $page->find('td.MainPane table tr');
+            if (count($tr)){
+                foreach ($tr as $row) {
+                    $td = $row->find('td.dataCell');
+                    
+                    
+                    $k=0;
+                    foreach ($td as $cell) {
+//                        if (!$cell->find('i',0) && trim($cell->plaintext)){
+                            print_r($k);
+//                            $result[$k] = ucfirst(trim($cell->plaintext));
+//                        }
+//                        if ($otd=$cell->find('span.separatorC',$key)){
+//                            $sub_otd_id=$key;
+//                            $result[$otd_id][$sub_otd_id]=$otd->plaintext;
+//                        }
+                        $k++;
                     }
                 }
-                $html->clear(); // подчищаем за собой
-                unset($html);
-                $start++;
             }
+            $page->clear();
+            echo '<pre>', print_r($result), '</pre>';
+            exit;
+            
+            
+//            while($start <= $end){
+//                $url = 'http://www.danceleague.ru/db.php?id=e18073,c'.$start;
+//                $html = file_get_html($url);
+//                
+//                
+//                
+//                if (count($html->find('p tr'))>1){
+//                    foreach($html->find('p tr') as $key_tr=>$tr){
+//                        if ($key_tr!=0){
+//                            if (count($tr->find('td'))<7){      // проверяем размер таблицы (соло - 5 колонок, пары -7)
+//                                foreach ($tr->find('td') as $key_td=>$td) {
+//                                    switch ($key_td){
+//                                        case 0:
+//                                            $category = $html->find('.dataCellRed.dottedRed');
+//                                            echo '<pre>', print_r($category), '</pre>';
+//                                            exit;
+//                                            $result[$start][$key_tr]['category'] = $category->plaintext;       // записываем название категории
+//                                            $result[$start][$key_tr]['class'] = $td->plaintext;
+//                                            break;
+//                                        case 1:
+//                                            
+//                                            $result[$start][$key_tr]['dancer1_name'] = $td->plaintext;
+//                                            break;
+//                                        case 2:
+//                                            $result[$start][$key_tr]['City'] = $td->plaintext;
+//                                            break;
+//                                        case 3:
+//                                            $result[$start][$key_tr]['club'] = $td->plaintext;
+//                                            break;
+//                                        case 4:
+//                                            $result[$start][$key_tr]['trener'] = $td->plaintext;
+//                                            break;      
+//                                    }
+//                                }       
+//                            } else {
+//                                
+//                            }
+//                            
+//                        }
+//                    }
+//                } 
+//                $start++;
+//            }
+//            echo '<pre>', print_r($result), '</pre>';
+//            exit;
         } else {
             return $this->render('index');
         }
-
-        
     }
-    
-    
-    
 }
 
 ?>
